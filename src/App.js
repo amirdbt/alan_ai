37ca6afc9b967c4d2e7cffa6dfc602897e642090
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2020-11-26 01:17:37 +0100

    Added alankey button for the aoo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey =
+  "b8bc6c5cb3a0a69bc0770c648fbc73bc2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("This code was executed");
+        }
+      },
+    });
+  }, []);
   return <div>Helo</div>;
 };
 
